4b735de2def9ec650183bf696c043feaebef0640
splashyin <yin.hao@rocketmail.com>
2016-08-13 20:45:53 +1200

    test

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $("#button_space").click(function() {
     $.ajax({
         url: 'http://api.open-notify.org/astros.json',
         type: 'GET',
-        dataType: 'json',
+        dataType: 'jsonp',
         success: function(){
             alert("h");
         }
