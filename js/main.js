808389978622d9f4e8e19c03ab8fd5187c4adaff
splashyin <yin.hao@rocketmail.com>
2016-09-03 22:52:51 +1200

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
+var Pokemon = (function () {
+    function Pokemon(id, name, order, abilities) {
+        this.poke_id = id;
+        this.poke_name = name;
+        this.poke_order = order;
+        this.poke_abilities = abilities;
+    }
+    return Pokemon;
+}());
+//A function to be called when the request succeed...
 var response = function (resp) {
-    var abi_stack = [];
+    var abi_stack;
     var i;
     for (i = 0; i < resp['abilities'].length; i++) {
         abi_stack.push(resp['abilities'][i].ability.name + "<br>");
     }
-    $('#pokemon-ability').html(abi_stack);
+    var aPokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
+    $('#pokemon-ability').html(aPokemon.poke_name);
 };
 $("#button_space").click(function () {
     var pokeindex = $('#pokeindex').val();
@@ -12,6 +23,6 @@ $("#button_space").click(function () {
         url: 'http://pokeapi.co/api/v2/pokemon/' + pokeindex,
         type: 'GET',
         dataType: 'json',
-        success: response
+        success: response //success(result, status, xhr) a function to be run when the request succeeds...    
     });
 });
--- js/main.ts
+++ js/main.ts
@@ -1,19 +1,38 @@
-var response = function(resp){ 
-    var abi_stack: any = [];
-    var i: number;
+class Pokemon {
+     poke_id: number;
+     poke_name: string;
+     poke_order: number;
+     poke_abilities: string[];
+
+     constructor(id: number, name: string, order: number, abilities: string[]){
+         this.poke_id = id;
+         this.poke_name = name;
+         this.poke_order = order;
+         this.poke_abilities = abilities;
+     }      
+}
+
+//A function to be called when the request succeed...
+var response: any = function(resp: any){ 
+    var abi_stack: string[];
+    var i: number; 
 
     for (i=0; i<resp['abilities'].length; i++){ 
         abi_stack.push(resp['abilities'][i].ability.name + "<br>"); 
     } 
-    $('#pokemon-ability').html(abi_stack); 
+
+    var aPokemon: Pokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
+
+    $('#pokemon-ability').html(aPokemon.poke_name); 
 }; 
  
 $("#button_space").click(function() { 
-     var pokeindex = $('#pokeindex').val(); 
+     var pokeindex: number = $('#pokeindex').val(); 
      $.ajax({ 
      url: 'http://pokeapi.co/api/v2/pokemon/' + pokeindex, 
      type: 'GET', 
      dataType: 'json', 
-     success: response     
+     success: response //success(result, status, xhr) a function to be run when the request succeeds...    
      });
 }); 
+
