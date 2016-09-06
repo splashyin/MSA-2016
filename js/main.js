dd495fe06e60b3b1d93008f0c2b50d5804e288bf
splashyin <yin.hao@rocketmail.com>
2016-09-06 19:38:57 +1200

    updates

--- js/main.js
+++ js/main.js
@@ -11,22 +11,27 @@ var Pokemon = (function () {
 var response = function (resp) {
     var abi_stack = [];
     var i;
+    var ablitemp;
+    var theAbli;
     for (i = 0; i < resp['abilities'].length; i++) {
-        abi_stack.push((i + 1) + ". " + resp['abilities'][i].ability.name + "<br>");
+        ablitemp = resp['abilities'][i].ability.name;
+        theAbli = ablitemp[0].toUpperCase() + ablitemp.slice(1);
+        abi_stack.push((i + 1) + ". " + theAbli + "<br>");
     }
     var aPokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
-    $('#pokemon-name').html(aPokemon.poke_name);
+    $('#pokemon-name').html(aPokemon.poke_name.toUpperCase());
     $('#pokemon-ability').html(aPokemon.poke_abilities);
 };
 //A function to be called when the request fails...
 var err_response = function (err_resp) {
+    alert("Wrong Input! Please enter Pokemon's Name or Index.");
     $('#pokemon-name').html("This pokemon is ...");
     $('#pokemon-ability').html("" + err_resp.statusText);
 };
 $("#find_button").click(function () {
     var pokeindex = $('#poke-input').val();
-    if (pokeindex <= 0) {
-        alert("Wrong Input! Please enter Pokemon's Name or Index.");
+    if (typeof pokeindex == "string") {
+        pokeindex = pokeindex.toLowerCase();
     }
     $.ajax({
         url: 'http://pokeapi.co/api/v2/pokemon/' + pokeindex,
--- js/main.ts
+++ js/main.ts
@@ -17,26 +17,31 @@ class Pokemon {
 var response: any = function(resp: any){ 
     var abi_stack: any = [];
     var i: number; 
-
+    var ablitemp: string;
+    var theAbli: string;
     for (i=0; i<resp['abilities'].length; i++){ 
-        abi_stack.push((i+1) + ". " + resp['abilities'][i].ability.name + "<br>"); 
+        ablitemp = resp['abilities'][i].ability.name;
+        theAbli = ablitemp[0].toUpperCase() + ablitemp.slice(1);
+
+        abi_stack.push((i+1) + ". " + theAbli + "<br>"); 
     } 
 
     var aPokemon: Pokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
-    $('#pokemon-name').html(aPokemon.poke_name);
+    $('#pokemon-name').html(aPokemon.poke_name.toUpperCase());
     $('#pokemon-ability').html(aPokemon.poke_abilities); 
 }; 
 
 //A function to be called when the request fails...
 var err_response: any = function(err_resp: any){
+    alert("Wrong Input! Please enter Pokemon's Name or Index.");
     $('#pokemon-name').html("This pokemon is ...");
     $('#pokemon-ability').html("" + err_resp.statusText);
 }
  
 $("#find_button").click(function() { 
      var pokeindex: any = $('#poke-input').val(); 
-     if(pokeindex <= 0){
-         alert("Wrong Input! Please enter Pokemon's Name or Index.");
+     if(typeof pokeindex == "string"){
+         pokeindex = pokeindex.toLowerCase();
      }
      $.ajax({ 
      url: 'http://pokeapi.co/api/v2/pokemon/' + pokeindex, 
