b40304a13338f3f5f845a49d51f2b2107a1387c1
splashyin <yin.hao@rocketmail.com>
2016-09-08 18:49:29 +1200

    hp-tesing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-//hide the image when everytime website is loaded
+//Clear the image when everytime website is loaded or refresed
 $(document).ready(function () {
     $("img").hide();
 });
 //Pokemon class, to construct an pokemon object
 var Pokemon = (function () {
-    function Pokemon(id, name, order, abilities) {
+    function Pokemon(id, name, order, abilities, attack, specialattck, hp) {
         this.poke_id = id;
         this.poke_name = name;
         this.poke_order = order;
         this.poke_abilities = abilities;
+        this.poke_attack = attack;
+        this.poke_hp = hp;
+        this.poke_specialattack = specialattck;
     }
     return Pokemon;
 }());
@@ -16,16 +19,37 @@ var Pokemon = (function () {
 function response(resp) {
     var abi_stack = [];
     var i;
+    var j;
     var ablitemp;
     var theAbli;
+    var pokemon_attack;
+    var pokemon_specialattack;
+    var pokemon_hp;
+    //get abilities...
     for (i = 0; i < resp['abilities'].length; i++) {
         ablitemp = resp['abilities'][i].ability.name;
         theAbli = ablitemp[0].toUpperCase() + ablitemp.slice(1);
         abi_stack.push((i + 1) + ". " + theAbli + "<br>");
     }
-    var aPokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
+    //get the stat of attack, special-attack and hp...
+    for (j = 0; j < resp['stats'].length; j++) {
+        if (resp['stats'][j].stat.name == "attack") {
+            pokemon_attack = resp['stats'][j].base_stat;
+        }
+        if (resp['stats'][j].stat.name == "hp") {
+            pokemon_hp = resp['stats'][j].base_stat;
+        }
+        if (resp['stats'][j].stat.name == "special-attack") {
+            pokemon_specialattack = resp['stats'][j].base_stat;
+        }
+    }
+    //Construct a new Pokemon!
+    var aPokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack, pokemon_attack, pokemon_hp, pokemon_specialattack);
     $('#pokemon-name').html(aPokemon.poke_name.toUpperCase());
     $('#pokemon-ability').html(aPokemon.poke_abilities);
+    $('#pokemon-hp').html("" + aPokemon.poke_hp);
+    //$('#pokemon-attack').html(""+ aPokemon.poke_attack);
+    //$('#pokemon-specialattck').html(""+ aPokemon.poke_specialattack);
     var image_url = resp['sprites'].front_default;
     $("#poke_image").show();
     $("#poke_image").attr("src", image_url);
--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,4 @@
-//hide the image when everytime website is loaded
+//Clear the image when everytime website is loaded or refresed
 $(document).ready(function(){
     $("img").hide();
 });
@@ -9,31 +9,73 @@ class Pokemon {
      poke_name: string;
      poke_order: number;
      poke_abilities: any;
+     poke_attack: number;
+     poke_specialattack: number;
+     poke_hp: number;
 
-     constructor(id: number, name: string, order: number, abilities: any){
+     constructor(id: number,
+                 name: string,
+                 order: number, 
+                 abilities: any,
+                 attack: number,
+                 specialattck: number,
+                 hp: number){
          this.poke_id = id;
          this.poke_name = name;
          this.poke_order = order;
          this.poke_abilities = abilities;
+         this.poke_attack = attack;
+         this.poke_hp = hp;
+         this.poke_specialattack = specialattck;
      }      
 }
 
 //A function to be called when the request succeed...
 function response(resp: any): void{
     var abi_stack: any = [];
-    var i: number; 
+    var i: number;
+    var j: number; 
     var ablitemp: string;
     var theAbli: string;
+    var pokemon_attack: number;
+    var pokemon_specialattack: number;
+    var pokemon_hp: number;
+
+    //get abilities...
     for (i=0; i<resp['abilities'].length; i++){ 
         ablitemp = resp['abilities'][i].ability.name;
         theAbli = ablitemp[0].toUpperCase() + ablitemp.slice(1);
 
         abi_stack.push((i+1) + ". " + theAbli + "<br>"); 
     } 
+    //get the stat of attack, special-attack and hp...
+    for(j=0; j<resp['stats'].length; j++){
+        if(resp['stats'][j].stat.name == "attack"){
+            pokemon_attack = resp['stats'][j].base_stat;
+        }
+        if(resp['stats'][j].stat.name == "hp"){
+            pokemon_hp = resp['stats'][j].base_stat;
+        }
+        if(resp['stats'][j].stat.name == "special-attack"){
+            pokemon_specialattack = resp['stats'][j].base_stat;
+        }
+    }
 
-    var aPokemon: Pokemon = new Pokemon(resp['id'], resp['name'], resp['order'], abi_stack);
+//Construct a new Pokemon!
+    var aPokemon: Pokemon = new Pokemon(
+        resp['id'],
+        resp['name'],
+        resp['order'],
+        abi_stack,
+        pokemon_attack,
+        pokemon_hp,
+        pokemon_specialattack);
+
     $('#pokemon-name').html(aPokemon.poke_name.toUpperCase());
     $('#pokemon-ability').html(aPokemon.poke_abilities);
+    $('#pokemon-hp').html(""+ aPokemon.poke_hp);
+    //$('#pokemon-attack').html(""+ aPokemon.poke_attack);
+    //$('#pokemon-specialattck').html(""+ aPokemon.poke_specialattack);
 
     var image_url: string = resp['sprites'].front_default;
     $("#poke_image").show();
