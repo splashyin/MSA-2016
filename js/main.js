58a19df71974f5ca3e78b53f393bae51812367d8
splashyin <yin.hao@rocketmail.com>
2016-09-15 16:36:50 +1200

    appinsights

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+"use strict";
+var appInsights = require("applicationinsights");
+appInsights.setup("<a5106a31-41bd-4144-8a83-644de01a9d95>");
 //Pokemon class, to construct an pokemon object
 var Pokemon = (function () {
     function Pokemon(id, name, order, abilities, attack, specialattack, hp) {
